9143a317a2885e5f1f79afd50c4e400727aa8b39
Ken Hawkins <khawkins98@gmail.com>
2019-09-25 19:46:18 +0200

    Spaceless should be safe string

--- tags/spaceless.js
+++ tags/spaceless.js
@@ -15,33 +15,27 @@
 // via https://github.com/mozilla/nunjucks/issues/207
 
 module.exports = function(nunjucksEngine){
+  this.tags = ['spaceless'];
 
-    function SpacelessExtension() {
+  this.parse = function (parser, nodes, lexer) {
+    var tok = parser.nextToken();
 
-      this.tags = ['spaceless'];
+    var args = parser.parseSignature(null, true);
+    parser.advanceAfterBlockEnd(tok.value);
 
-      this.parse = function (parser, nodes, lexer) {
-        var tok = parser.nextToken();
-
-        var args = parser.parseSignature(null, true);
-        parser.advanceAfterBlockEnd(tok.value);
-
-        var body = parser.parseUntilBlocks('error','endspaceless');
-        var errorBody = null;
-        if(parser.skipSymbol('error')) {
-            parser.skip(lexer.TOKEN_BLOCK_END);
-            errorBody = parser.parseUntilBlocks('endremote');
-        }
-
-        parser.advanceAfterBlockEnd();
-
-        return new nodes.CallExtension(this, 'run', args, [body, errorBody]);
-      };
-
-      this.run = function (context, body) {
-        return body().replace(/\s+/g, ' ').replace(/>\s</g, '><');
-      };
+    var body = parser.parseUntilBlocks('error','endspaceless');
+    var errorBody = null;
+    if(parser.skipSymbol('error')) {
+        parser.skip(lexer.TOKEN_BLOCK_END);
+        errorBody = parser.parseUntilBlocks('endremote');
     }
 
-    return new SpacelessExtension();
+    parser.advanceAfterBlockEnd();
+
+    return new nodes.CallExtension(this, 'run', args, [body, errorBody]);
+  };
+
+  this.run = function (context, body) {
+    return new nunjucksEngine.runtime.SafeString(body().replace(/\s+/g, ' ').replace(/>\s</g, '><'));
+  };
 };
