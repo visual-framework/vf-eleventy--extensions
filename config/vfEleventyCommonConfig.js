ee89f937fe80c477b6909338ebf0dec96019fd2b
Ken Hawkins <khawkins98@gmail.com>
2019-10-25 12:27:16 +0200

    Fractal shouldn't prompt 11ty to rebuild for precompiled templates

--- config/vfEleventyCommonConfig.js
+++ config/vfEleventyCommonConfig.js
@@ -5,7 +5,14 @@ module.exports = function(config) {
   // Watch for changes, we'll use this to trigger a fractal rebuild
   const touch = require("touch");
   fractal.components.on('updated', function(source, eventData){
-  	console.log('Component source has been updated: ' + source.path);
+    // don't do anything if it's a precompiled template/asset that's updated
+    if (source.path.indexOf('.precompiled.js') > 0 ||
+        source.path.indexOf('package.variables.scss') > 0) 
+    {
+      return;
+    }
+
+    console.log('Component source has been updated: ' + source.path);  
 
     // For now we're just touching a watched file until we can figure out something better
     // https://github.com/11ty/eleventy/issues/604
